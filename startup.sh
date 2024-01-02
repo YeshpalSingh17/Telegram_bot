2df38e5145a5c45d05cad7d225f5f463ac3694e5
YeshpalSingh17 <yeshpal1709@gmail.com>
2024-01-02 16:54:24 +0530

    bot

--- startup.sh
+++ startup.sh
@@ -1,5 +1,2 @@
 #!/bin/bash
-
-pip install --no-cache-dir -r /home/site/wwwroot/requirements.txt
-
-chmod +x startup.sh
+pip install -r requirements.txt
--- wsgi.py
+++ wsgi.py
@@ -1,2 +1,10 @@
 import os
-from bot import bot as application
+from bot import bot 
+
+def application(environ, start_response):
+    if environ['REQUEST_METHOD'] == 'POST':
+        update = bot.updates_handler.process_new_updates([Update.de_json(environ["wsgi.input"].read())])[0]
+        return ""
+    else:
+        start_response('200 OK', [('Content-Type', 'text/html')])
+        return [b"Hello World"]
